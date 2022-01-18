0e01d02ba2c0b13fa70406b9e6de504657595947
vitaliizaichuk <zaychuk.t@gmail.com>
2022-01-18 20:28:55 +0000

    update burger

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 "use strict"
+
+const burgerMenu = document.querySelector('header__burger');
+if (burgerMenu) {
+    const headerMenu = document.querySelector('header__menu');
+    burgerMenu.addEventListener("click", function(e) {
+       burgerMenu.classList.toggle('_active');
+       headerMenu.classList.toggle('_active');
+    });
+}
