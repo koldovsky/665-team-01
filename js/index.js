e8ae873111b0f73e0b1c521af454238694bf104a
Vitalii Zaichuk <vitaliizaichuk@VZ16.local>
2022-01-20 22:45:19 +0200

    vscode

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
-"use strict"
+(function() {
+    "use strict";
 
-const burgerMenu = document.querySelector('.header__burger');
-const socialMenu = document.querySelector('.social__menu');
-if (burgerMenu) {
+    const burgerMenu = document.querySelector('.header__burger');
+    const socialMenu = document.querySelector('.social__menu');
     const headerMenu = document.querySelector('.header__menu');
-    burgerMenu.addEventListener("click", function(e) {
-       document.body.classList.toggle('lock'); 
-       burgerMenu.classList.toggle('active');
-       headerMenu.classList.toggle('active');
-       socialMenu.classList.toggle('active');
+        burgerMenu.addEventListener("click", function(e) {
+           document.body.classList.toggle('lock'); 
+           burgerMenu.classList.toggle('active');
+           headerMenu.classList.toggle('active');
+        socialMenu.classList.toggle('active');
     });
-}
+})();
