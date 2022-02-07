eae222b4a37c5fe3548c9e27d2a969af989bf826
ViacheslavTrofymenko <trofymenko.v.n@gmail.com>
2022-02-07 15:35:38 +0200

    product list best prices updated

--- js/product-list-best-prices.js
+++ js/product-list-best-prices.js
@@ -1,33 +1,32 @@
 (function () {
     let products;
-    let rate =1;    
+    let rate = 1;
     async function loadProducts() {
         const response = await fetch('products-accordion-best-prices.json');
         products = await response.json();
         renderProducts();
     }
 
-    function renderProducts() {      
-            for (const product of products) {            
+    function renderProducts() {        
+        for (const product of products) {
             const productsCardBody = `
-        <div class="best-prices__product-card">
-          <a href="#" class="best-prices__product-card-link">
-            <img src="${product.imgUrl}" alt="${product.title}
-              class="best-prices__product-card-photo" />
-            <p class="best-prices__product-card-name">
-              ${product.title}
-            </p>
-          </a>
-          <p class="best-prices__product-card-price">${(product.price * rate).toFixed(2)}</p>
-          <div class="add-to-cart__btn">
-            <a href="#">Add to Cart</a>
-          </div>
-        </div>`;              
+                <div class="best-prices__product-card">
+                    <a href="#" class="best-prices__product-card-link">
+                        <img src="${product.imgUrl}" alt="${product.title}
+                        class="best-prices__product-card-photo" />
+                        <p class="best-prices__product-card-name">${product.title}</p>
+                    </a>
+                    <p class="best-prices__product-card-price">${(product.price * rate).toFixed(2)}</p>
+                    <div class="add-to-cart__btn">
+                        <a href="#">Add to Cart</a>
+                    </div>
+                </div>`;
+
             if (product.id < 5) {
-               let productsContainer = document.querySelector('.content-electric-guitars');                
+                productsContainer = document.querySelector('.content-electric-guitars');
                 productsContainer.innerHTML += productsCardBody;
             } else if (product.id >= 5 && product.id < 9) {
-                 productsContainer = document.querySelector('.content-acoustic-guitars');
+                productsContainer = document.querySelector('.content-acoustic-guitars');
                 productsContainer.innerHTML += productsCardBody;
             } else if (product.id >= 9 && product.id < 13) {
                 productsContainer = document.querySelector('.content-drums');
@@ -62,11 +61,12 @@
     document.querySelector('.best-prices__convert').addEventListener('click', convertCurrency);
 
     async function convertCurrency() {
-       const response = await fetch('https://api.exchangerate-api.com/v4/latest/USD')
-       const currencies = await response.json();
-       const convertTo = document.querySelector('.best-prices__currency').value;
-       rate = currencies.rates[convertTo];
-       renderProducts();
+        const response = await fetch('https://api.exchangerate-api.com/v4/latest/USD')
+        const currencies = await response.json();
+        const convertTo = document.querySelector('.best-prices__currency').value;
+        rate = currencies.rates[convertTo];
+        productsContainer.innerHTML = '';
+        renderProducts();
     }
 
     loadProducts();
