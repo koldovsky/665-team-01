99c42eda2d5a4f584c0d56628ed555e60df218e8
ViacheslavTrofymenko <trofymenko.v.n@gmail.com>
2022-02-07 13:04:47 +0200

    added carousel to best selling block

--- js/best-selling-carousel.js
+++ js/best-selling-carousel.js
@@ -1,45 +1,84 @@
 (function () {
-
-    const products = [
-        {
-            id: "1",
-            title: "Ericson Black&White Snare Drum",
-            imgUrl: "img/drum.jpeg",
-            price: 180.99
-        },
-        {
-            id: "2",
-            title: "Winston WR-100 Electric Guitar",
-            imgUrl: "img/red-guitar.jpeg",
-            price: 320.99
-        },
-        {
-            id: "3",
-            title: "Jackson RI 405 Electric Guitar",
-            imgUrl: "img/guitar.jpeg",
-            price: 300.99
-        },
-        {
-            id: "4",
-            title: "Jiwa Studio JWL100 Single Row Tambourine",
-            imgUrl: "img/tamb.jpeg",
-            price: 50.99
-        }
-    ];
-
-    function renderProducts(products) {
-        const productsContainer = document.querySelector('.best-selling-cards__container');
-        for (const product of products) {
-            productsContainer.innerHTML += `
-        <div class="best-selling__card">
-              <img class="preview-img" alt="${product.title}" src="${product.imgUrl}">
-              <p class="best-selling__subheading">${product.title}</p>
-              <div class="best-selling__price">${product.price}</div>
+    const slides = [
+        `<div class="best-selling__card">
+              <img class="preview-img" alt="Ericson Black&White Snare Drum" src="img/drum.jpeg">
+              <p class="best-selling__subheading">Ericson Black&White Snare Drum</p>
+              <div class="best-selling__price">
+                <p>$115.00</p>
+              </div>
               <div class="add-to-cart-best-selling__btn">
                 <a href="#">Add to Cart</a>
               </div>
-        </div>`;
+            </div>`,
+        `<div class="best-selling__card">
+              <img class="preview-img" alt="Winston WR-100 Electric Guitar" src="img/red-guitar.jpeg">
+              <p class="best-selling__subheading">Winston WR-100 Electric Guitar</p>
+              <div class="best-selling__price">
+                <p>$320.00</p>
+              </div>
+              <div class="add-to-cart-best-selling__btn">
+                <a href="#">Add to Cart</a>
+              </div>
+            </div>`,
+        `<div class="best-selling__card">
+              <img class="preview-img" alt="Jackson RI 405 Electric Guitar" src="img/guitar.jpeg">
+              <p class="best-selling__subheading">Jackson RI 405 Electric Guitar</p>
+              <div class="best-selling__price">
+                <p>$300.00</p>
+              </div>
+              <div class="add-to-cart-best-selling__btn">
+                <a href="#">Add to Cart</a>
+              </div>
+            </div>`,
+        `<div class="best-selling__card">
+              <img class="preview-img" alt="Jiwa Studio JWL100 Single Row Tambourine" src="img/tamb.jpeg">
+              <p class="best-selling__subheading">Jiwa Studio JWL100 Single Row Tambourine</p>
+              <div class="best-selling__price">
+                <p>$50.00</p>
+              </div>
+              <div class="add-to-cart-best-selling__btn">
+                <a href="#">Add to Cart</a>
+              </div>
+            </div>`
+    ];
+
+    let currentSlideIdx = 0;
+
+    function showCurrentSlide() {
+        const slideContainer = document.querySelector('.best-selling__carousel-slide-container');
+        slideContainer.innerHTML = slides[currentSlideIdx];
+        if (window.innerWidth >= 768) {
+            const secondSlideIdx = currentSlideIdx + 1 >= slides.length ? 0 : currentSlideIdx + 1;
+            slideContainer.innerHTML += slides[secondSlideIdx];
+            if (window.innerWidth >= 850) {
+                const thirdSlideIdx = secondSlideIdx + 1 >= slides.length ? 0 : secondSlideIdx + 1;
+                slideContainer.innerHTML += slides[thirdSlideIdx];
+                if (window.innerWidth >= 992) {
+                    const fourSlideIdx = thirdSlideIdx + 1 >= slides.length ? 0 : thirdSlideIdx + 1;
+                    slideContainer.innerHTML += slides[fourSlideIdx];
+                    // const fiveSlideIdx = fourSlideIdx + 1 >= slides.length ? 0 : fourSlideIdx + 1;
+                    // slideContainer.innerHTML += slides[fiveSlideIdx];
+                    // const sixSlideIdx = fiveSlideIdx + 1 >= slides.length ? 0 : fiveSlideIdx + 1;
+                    // slideContainer.innerHTML += slides[sixSlideIdx];
+                }
+            }
         }
     }
-    renderProducts(products);
+
+    function showNextSlide() {
+        currentSlideIdx = currentSlideIdx + 1 >= slides.length ? 0 : currentSlideIdx + 1;
+        showCurrentSlide();
+    }
+
+    function showPrevSlide() {
+        currentSlideIdx = currentSlideIdx - 1 < 0 ? slides.length - 1 : currentSlideIdx - 1;
+        showCurrentSlide();
+    }
+
+    showCurrentSlide();
+
+    document.querySelector('.best-selling__carousel-forward').addEventListener("click", showNextSlide);
+    document.querySelector('.best-selling__carousel-back').addEventListener("click", showPrevSlide);
+
+    window.addEventListener('resize', showCurrentSlide);
 })();
