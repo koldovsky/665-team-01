955010694bb33fa4f58cb5904dd8feb17005593b
ViacheslavTrofymenko <trofymenko.v.n@gmail.com>
2022-02-08 22:35:43 +0200

    updated js for best prices block

--- js/product-list-best-prices.js
+++ js/product-list-best-prices.js
@@ -1,15 +1,20 @@
 (function () {
     let products;
     let rate = 1;
+    let currentCategory = 'electric-guitar';
+
     async function loadProducts() {
         const response = await fetch('products-accordion-best-prices.json');
         products = await response.json();
         renderProducts();
     }
 
-    function renderProducts() {        
-        for (const product of products) {
-            const productsCardBody = `
+    function renderProducts() {
+        const productsContainer = document.querySelector('.best-prices__product-cards');
+        productsContainer.innerHTML = '';
+        const productsToShow = products.filter(productCard => productCard.category === currentCategory);
+        for (const product of productsToShow) {            
+            productsContainer.innerHTML += `
                 <div class="best-prices__product-card">
                     <a href="#" class="best-prices__product-card-link">
                         <img src="${product.imgUrl}" alt="${product.title}
@@ -21,54 +26,30 @@
                         <a href="#">Add to Cart</a>
                     </div>
                 </div>`;
-
-            if (product.id < 5) {
-                productsContainer = document.querySelector('.content-electric-guitars');
-                productsContainer.innerHTML += productsCardBody;
-            } else if (product.id >= 5 && product.id < 9) {
-                productsContainer = document.querySelector('.content-acoustic-guitars');
-                productsContainer.innerHTML += productsCardBody;
-            } else if (product.id >= 9 && product.id < 13) {
-                productsContainer = document.querySelector('.content-drums');
-                productsContainer.innerHTML += productsCardBody;
-            } else if (product.id >= 13 && product.id < 17) {
-                productsContainer = document.querySelector('.content-orchestric');
-                productsContainer.innerHTML += productsCardBody;
-            } else if (product.id >= 17 && product.id < 21) {
-                productsContainer = document.querySelector('.content-other');
-                productsContainer.innerHTML += productsCardBody;
-            }
         }
     }
 
     const tabs = document.querySelectorAll('.tab-nav');
-    const contents = document.querySelectorAll('.contents');
-
     for (let i = 0; i < tabs.length; i++) {
         tabs[i].addEventListener('click', (event) => {
-            for (let j = 0; j < tabs.length; j++) {
-                tabs[j].classList.remove('tab-nav-active');
-            }
+        for (let j = 0; j < tabs.length; j++) {
+            tabs[j].classList.remove('tab-nav-active');
             event.target.classList.add('tab-nav-active');
-
-            for (let k = 0; k < contents.length; k++) {
-                contents[k].classList.remove('content-active');
-            }
-            contents[i].classList.add('content-active');
-        })
-    }
-
-    document.querySelector('.best-prices__convert').addEventListener('click', convertCurrency);
-
-    async function convertCurrency() {
-        const response = await fetch('https://api.exchangerate-api.com/v4/latest/USD')
-        const currencies = await response.json();
-        const convertTo = document.querySelector('.best-prices__currency').value;
-        rate = currencies.rates[convertTo];
-        productsContainer.innerHTML = '';
+        }
+        currentCategory = tabs[i].id;
         renderProducts();
-    }
+    });
+}
+document.querySelector('.best-prices__convert').addEventListener('click', convertCurrency);
 
-    loadProducts();
+async function convertCurrency() {
+    const response = await fetch('https://api.exchangerate-api.com/v4/latest/USD')
+    const currencies = await response.json();
+    const convertTo = document.querySelector('.best-prices__currency').value;
+    rate = currencies.rates[convertTo];
+    renderProducts();
+}
+
+loadProducts();
 
 })();
